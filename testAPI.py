6e64343d0aeb367fa9ed485265216f9a8d120ad5
LosFouchos <tahina.foucher@outlook.com>
2022-09-16 14:44:19 +0200

    Create testAPI.py

---
+++ testAPI.py
@@ -0,0 +1,7 @@
+import requests
+
+r = requests.post(' https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/exec', data={'cle': 'CLE-TEST-IOT', 'donnees' : ['tahina.foucher@outlook.com','16/09/2022','urlRelais','Coucou Ouivalo, j ai fais mon possible']})
+r.status_code
+print(r.text)
+r.json()
+
