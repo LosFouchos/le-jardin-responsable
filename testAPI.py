8bcd8b6ec5bdca49979e893d2e1af4c37a16644e
LosFouchos <tahina.foucher@outlook.com>
2022-09-16 14:46:20 +0200

    Update testAPI.py

--- testAPI.py
+++ testAPI.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.post(' https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/exec', data={'cle': 'CLE-TEST-IOT', 'donnees' : ['tahina.foucher@outlook.com','16/09/2022','urlRelais','Coucou Ouivalo, j ai fais mon possible']})
+r = requests.post(' https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/exec', data={'cle': 'CLE-TEST-IOT', 'donnees' : ['tahina.foucher@outlook.com','16/09/2022','https://github.com/LosFouchos/le-jardin-responsable','Coucou Ouivalo, j ai fais mon possible :)']})
 r.status_code
 print(r.text)
 r.json()
