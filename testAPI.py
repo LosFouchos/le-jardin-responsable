b62b752cfc0fd53e4ced5b3ea70f16974c5ac211
LosFouchos <tahina.foucher@outlook.com>
2022-09-19 12:22:22 +0200

    Update testAPI.py

--- testAPI.py
+++ testAPI.py
@@ -2,7 +2,7 @@
 import requests
 import json
 url =  "https://script.google.com/macros/s/AKfycby-TJmFFUFTfiNUbMoSIZx8LVtiskQ-bUt4xO6hmrU0XQpJS8IPUBow/exec"
-data = {'cle': 'CLE-TEST-IOT', 'donnees' : ['tahina.foucher@outlook.com','16/09/2022','https://github.com/LosFouchos/le-jardin-responsable','Coucou Ouivalo, j ai fais mon possible :)']}
+data = {'cle': 'CLE-TEST-IOT', 'donnees' : ['tahina.foucher@outlook.com','16/09/2022','https://github.com/LosFouchos/le-jardin-responsable','Coucou Ouivalo, j ai fais mon possible :) Hate de vous rencontrer']}
 
 r = requests.post(url, data=json.dumps(data))
 
